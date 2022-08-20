8a3b982de0415a24603fea10cfc3d4839dba5140
agricola4017 <michaelchen4017@gmail.com>
2022-08-19 20:08:48 -0400

    overhead django project + testapp, tempalte/view part of MVC

--- FrontEnd/django_project/urls.py
+++ FrontEnd/django_project/urls.py
@@ -1,4 +1,4 @@
-"""example_django_proj URL Configuration
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls")),
-    path("posts", include("posts.urls"))
+    path("", include("testapp.urls"))
 ]
